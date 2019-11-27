777af3b6d3dfee91d25b00e4974ce93c10de27fe
gonggaobushang <47070965+gonggaobushang@users.noreply.github.com>
2019-11-27 09:14:49 +0800

    Update xgboost.R

--- xgboost.R
+++ xgboost.R
@@ -82,3 +82,33 @@ bst <- xgb.train(data=dtrain,
                  eval_metric = "ndcg",
                  nthread = 2, 
                  objective = "binary:logistic")
+
+#2019.11.27学习笔记
+require(xgboost)
+data(agaricus.train, package='xgboost')
+data(agaricus.test, package='xgboost')
+dtrain <- xgb.DMatrix(agaricus.train$data, label = agaricus.train$label)
+dtest <- xgb.DMatrix(agaricus.test$data, label = agaricus.test$label)
+nround <- 2
+param <- list(max_depth=2, eta=1, silent=1, nthread=2, objective='binary:logistic')
+xgb.cv(param, dtrain, nround,
+       nfold=5, #原始集被随机分成等分 
+       metrics={'error'}) #评估指标
+xgb.cv(param, dtrain, nround, nfold=5,
+       metrics='error', showsd = FALSE)
+#自定义目标函数和损失
+logregobj <- function(preds, dtrain) {
+  labels <- getinfo(dtrain, "label")
+  preds <- 1/(1 + exp(-preds))
+  grad <- preds - labels
+  hess <- preds * (1 - preds)
+  return(list(grad = grad, hess = hess))
+}
+evalerror <- function(preds, dtrain) {
+  labels <- getinfo(dtrain, "label")
+  err <- as.numeric(sum(labels != (preds > 0)))/length(labels)
+  return(list(metric = "error", value = err))
+}
+param <- list(max_depth=2, eta=1, silent=1,
+              objective = logregobj, eval_metric = evalerror)
+xgb.cv(params = param, data = dtrain, nrounds = nround, nfold = 5)
